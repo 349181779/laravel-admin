d11a58c16b3333abac2a11b5c1929288e2f3e5f4
baobao <bao@yizhan18.com>
2015-07-29 12:30:07 +0800

    修改首页

--- resources/views/home/app/index.blade.php
+++ resources/views/home/app/index.blade.php
@@ -61,12 +61,7 @@
 							  </ul>
 							  <div class="clear"></div>
 						</div>
-						 <div class="page_bt">
-				          <ul>
-						   <?php echo $all_app->render(); ?>
-						 </ul>
-						 <div class="clear"></div>
-		               </div>
+						 
 					   <!---->
 				  </div>
 				  <!---->
@@ -78,6 +73,12 @@
 		   </div>
 	</div>
 </div>
+<div class="page_bt">
+				          <ul>
+						   <?php echo $all_app->render(); ?>
+						 </ul>
+						 <div class="clear"></div>
+		               </div>
 <!-- end main -->
 <!-- footer -->
 @section('footer')
