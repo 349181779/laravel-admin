75a1579f6f312cd7de95e8221bb8ce2f97d33bb1
yangyifan <547484196@qq.com>
2015-08-16 20:35:45 +0800

    -a

--- app/Model/User/SiteCatModel.php
+++ app/Model/User/SiteCatModel.php
@@ -17,5 +17,22 @@ class SiteCatModel extends BaseModel {
     protected $table    = 'user_site_cat';//定义表名
     protected $guarded  = ['id'];//阻挡所有属性被批量赋值
 
+    /**
+     * 获得全部文章分类--无限极分类（编辑菜单时选项）
+     *
+     * @descript  递归组合无限极分类，为了编辑页面和增加页面select 展示
+     * @param $name 表单name名称
+     * @param $id 当前id
+     * @return array
+     * @author yangyifan <yangyifanphp@gmail.com>
+     */
+    public static function getAllForSchemaOption($name, $id = 0, $first = true){
+        //加载函数库
+        load_func('common');
+        $data = $id > 0 ? merge_tree_node(obj_to_array(self::where('id', '<>' , $id)->where('user_info_id', '=', is_user_login())->get())) : merge_tree_node(obj_to_array(self::all()));
+        $first == true && array_unshift($data, ['id' => '0', $name => '顶级分类']);
+        return $data;
+    }
+
 
 }
