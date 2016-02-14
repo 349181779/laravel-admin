639c81123869d6d7863e71b661853e9c306c169a
Yang Yifan <yangyifanphp@gmail.com>
2016-02-14 12:59:20 +0800

    style(html_build):删除多余模板文件

--- resources/views/admin/html_builder/list.blade.php
+++
@@ -1,15 +0,0 @@
-@section('header')
-    @include('admin.block.header')
-    <link href="<?php echo elixir('dist/builder_list.css');?>" rel="stylesheet" >
-    <style>
-        .fixed-table-toolbar .search{padding:0;width: auto;}
-    </style>
-@show
-@include('admin.block.body')
-@include('admin.html_builder.list_form')
-<script>
-    var tableName = '<?php echo $table_name ;?>';
-</script>  
-<script src="<?php echo elixir('dist/builder_list.js');?>"></script> 
-@include('admin.block.footer')
-</html>
